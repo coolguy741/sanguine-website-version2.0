a8f95d23ad2ff8165962caf692001c3a61ae8b19
reinforest915820 <alexandre915820@gmail.com>
2022-12-18 13:40:31 -0500

    completed the work page

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Work() {
   return (
     <>
-        <div className='flex-row sm: flex-col'>
-          
+        <div className='flex'>
+          <Link to = "/work/pornbrain"><img className="h-screen w-[33.33vw] flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/BRAIN.png"/></Link>
+          <Link to = "/work/spotleukaemia"><img className="h-screen w-[33.33vw] flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/Spot Leukaemia.png"/></Link>
+          <Link to = "/work/futureofsurgery"><img className="h-screen w-[33.33vw] flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/SURGERY CONFERENCE.png"/></Link>
         </div>
     </>
   );
