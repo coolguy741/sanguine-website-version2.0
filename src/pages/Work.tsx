32d88152cc727a81a59c59854489396e75949855
reinforest915820 <alexandre915820@gmail.com>
2022-12-18 13:46:40 -0500

    [fix] responsive page

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 function Work() {
   return (
     <>
-        <div className='flex'>
-          <Link to = "/work/pornbrain"><img className="h-screen w-[33.33vw] flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/BRAIN.png"/></Link>
-          <Link to = "/work/spotleukaemia"><img className="h-screen w-[33.33vw] flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/Spot Leukaemia.png"/></Link>
-          <Link to = "/work/futureofsurgery"><img className="h-screen w-[33.33vw] flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/SURGERY CONFERENCE.png"/></Link>
+        <div className='lg:flex flex-column'>
+          <Link to = "/work/pornbrain"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/BRAIN.png"/></Link>
+          <Link to = "/work/spotleukaemia"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/Spot Leukaemia.png"/></Link>
+          <Link to = "/work/futureofsurgery"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/SURGERY CONFERENCE.png"/></Link>
         </div>
     </>
   );
