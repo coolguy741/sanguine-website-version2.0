c1d5c95a5ddbb83a91fe5b4ca5befc27f3341741
reinforest915820 <alexandre915820@gmail.com>
2022-12-20 07:27:39 -0500

    [feat] complete the work page responsive

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Work() {
   return (
     <>
-        <div className='lg:flex flex-column'>
+        <div className='lg:flex flex-column overflow-hidden'>
           <Link to = "/work/pornbrain"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/BRAIN.png"/></Link>
           <Link to = "/work/spotleukaemia"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/Spot Leukaemia.png"/></Link>
           <Link to = "/work/futureofsurgery"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/SURGERY CONFERENCE.png"/></Link>
