0b8c190092087a957ee0a07b4b1849ccfb04d06f
reinforest915820 <alexandre915820@gmail.com>
2022-12-20 11:22:48 -0500

    [fix] a tag- overflow hidden

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 function Work() {
   return (
     <>
-        <div className='lg:flex flex-column overflow-hidden'>
-          <Link to = "/work/pornbrain"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/BRAIN.png" alt='brain'/></Link>
-          <Link to = "/work/spotleukaemia"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/Spot Leukaemia.png" alt="Spot Leukaemia"/></Link>
-          <Link to = "/work/futureofsurgery"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/SURGERY CONFERENCE.png" alt="Future of Surgery"/></Link>
+        <div className='flex flex-col lg:flex-row'>
+          <Link className="overflow-hidden" to = "/work/pornbrain"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/BRAIN.png" alt='brain'/></Link>
+          <Link className="overflow-hidden" to = "/work/spotleukaemia"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/Spot Leukaemia.png" alt="Spot Leukaemia"/></Link>
+          <Link className="overflow-hidden" to = "/work/futureofsurgery"><img className="lg:h-screen lg:w-[33.33vw] h-[33vh] w-full flex-initial object-cover transition ease-linear hover:scale-110 duration-500" src="/image/SURGERY CONFERENCE.png" alt="Future of Surgery"/></Link>
         </div>
     </>
   );
