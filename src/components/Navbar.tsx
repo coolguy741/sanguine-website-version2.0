05fde81b668ff62fffff753fbd961704737f0b29
reinforest915820 <alexandre915820@gmail.com>
2022-12-20 07:22:20 -0500

    [feat] completed the Navbar responsive and completed the static not responsive services page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-      <nav className='fixed w-full flex justify-between items-center px-10 pt-12 z-50'>
-        <img className="w-40" src='SANGUINE_LOGO_WHITE.svg' alt="logo"/>
-        <ul className='flex text-white text-lg'>
-          <li className='px-3 active:font-medium'><Link to="/">HOME</Link></li>
-          <li className='px-3'><Link to="/work">WORK</Link></li>
-          <li className='px-3'><Link to="/about">ABOUT</Link></li>
-          <li className='px-3'><Link to="/services">SERVICES</Link></li>
+      <nav className='fixed w-full flex flex-wrap justify-between items-center sm:px-10 sm:pt-12 pt-4 z-50 px-2 '>
+        <img className="w-40 pb-4" src='SANGUINE_LOGO_WHITE.svg' alt="logo"/>
+        <ul className='flex flex-wrap text-white text-lg'>
+          <li className='sm:px-3 pr-2 active:font-medium'><Link to="/">HOME</Link></li>
+          <li className='sm:px-3 pr-2'><Link to="/work">WORK</Link></li>
+          <li className='sm:px-3 pr-2'><Link to="/about">ABOUT</Link></li>
+          <li className='sm:px-3 pr-2'><Link to="/services">SERVICES</Link></li>
         </ul>
       </nav>
   );
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Services() {
   return (
     <>
-      <div className="flex justify-center bg-black text-white md:h-[70vh] p-8 pt-16">
+      <div className="flex justify-center bg-black text-white md:h-[70vh] p-8 pt-32">
         <div className='my-auto'>
           <h6 className='font-medium text-lg text-center'>
             Our team has provided advertising and PR consultancy to an array of
@@ -18,23 +18,23 @@ function Services() {
           </div>
         </div>
       </div>
-      {/* <div className="md:flex p-7 flex-column">
+      <div className="md:flex p-7 flex-column">
         <div className="md:basis-1/2 pb-4 px-2">
           <h4 className="text-lg font-medium md:text-4xl">Let's work together</h4>
           <p className="pt-4">
-            <i className="fa fa-envelope-o" aria-hidden="true"/>
+            <i className="fa fa-envelope-o pr-1" aria-hidden="true"></i>
             info@sanguineagency.com
           </p>
         </div>
-        <div className="md: basis-1/2 px-2">
+        <div className="md: basis-1/2 px-2 flex-col">
           <div className="panel">
             <div className="desc-item">
-              <p><b>INVESTOR FILMS</b></p>
+              <h6 className='desc-item-heading'><b>INVESTOR FILMS</b></h6>
               <p>We produce succinct hype films to electrify investors.</p>
             </div>
             <div className="desc-item">
-              <p><b>WEB DESIGN &amp; DEVELOPMENT</b></p>
-              <p>
+              <h6 className='desc-item-heading'><b>WEB DESIGN &amp; DEVELOPMENT</b></h6>
+              <p className='desc-item-content'>
                 We design and build elegant, dynamic and technologically advanced
                 websites.
               </p>
@@ -42,15 +42,15 @@ function Services() {
           </div>
           <div className="panel">
             <div className="desc-item">
-              <p><b>CREATIVE ADVERTISING</b></p>
-              <p>
+              <h6 className='desc-item-heading'><b>CREATIVE ADVERTISING</b></h6>
+              <p className='desc-item-content'>
                 We conceive and execute 360 advertising campaigns for all media
                 channels.
               </p>
             </div>
             <div className="desc-item">
-              <p><b>PR CONSULTANCY</b></p>
-              <p>
+              <h6 className='desc-item-heading'><b>PR CONSULTANCY</b></h6>
+              <p className='desc-item-content'>
                 We deliver strategies to increase brand awareness and generate
                 brand talkability.
               </p>
@@ -58,22 +58,22 @@ function Services() {
           </div>
           <div className="panel">
             <div className="desc-item">
-              <p><b>BRAND DEVELOPMENT</b></p>
-              <p>
+              <h6 className='desc-item-heading'><b>BRAND DEVELOPMENT</b></h6>
+              <p className='desc-item-content'>
                 We devise brand toolkits, comprising a logo, a manifesto, and tone
                 of voice guidelines.
               </p>
             </div>
             <div className="desc-item">
-              <p><b>AUDIENCE RESEARCH</b></p>
-              <p>
+              <h6 className='desc-item-heading'><b>AUDIENCE RESEARCH</b></h6>
+              <p className='desc-item-content'>
                 We perform rigorous scientific studies of target audiences,
                 identifying the key factors that most influence decision-making.
               </p>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
